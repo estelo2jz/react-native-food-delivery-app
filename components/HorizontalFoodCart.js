d944203b4a176bbb76346f643a4f26e457d5ac2f
esteloabell <manitobaaa@gmail.com>
2021-08-02 22:14:04 -0500

    Create HorizontalFoodCart.js

---
+++ components/HorizontalFoodCart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HorizontalFoodCart = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default HorizontalFoodCart
+
+const styles = StyleSheet.create({})
