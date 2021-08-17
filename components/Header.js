664e5342d8d7e07749f7213c8aba533a10da519c
esteloabell <manitobaaa@gmail.com>
2021-08-17 14:12:50 -0500

    had to rewrite because, i was trying to figure out how to fixed Ranimated 2 falied to create a wrorklet

--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {FONTS} from '../constants';
+import { 
+    View,
+    Text
+} from 'react-native';
+import { FONTS } from "../constants"
 
-const Header = ({ containerStyle, title, leftComponent, rightComponent }) => {
-  return (
-    <View
-      style={{
-        flexDirection: 'row',
-        ...containerStyle,
-      }}
-    >
-      {/* Left */}
-      {leftComponent}
+const Header = ({ containerStyle, title, titleStyle, leftComponent, rightComponent }) => {
+    return (
+        <View
+            style={{
+                height: 60,
+                flexDirection: 'row',
+                ...containerStyle
+            }}
+        >
+            {
+                leftComponent
+            }
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={{  ...FONTS.h3, ...titleStyle}}>{title}</Text>
+            </View>
+            {
+                rightComponent
+            }
 
-      {/* Title */}
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Text style={{...FONTS.h3}}>
-          {title}
-        </Text>
-      </View>
-
-      {/* Right */}
-      {rightComponent}
-    </View>
-  )
+        </View>
+    )
 }
 
-export default Header;
+export default Header
--- components/index.js
+++ components/index.js
@@ -1,7 +1,17 @@
-import Header from './Header';
-import HorizontalFoodCart from './HorizontalFoodCart';
+import Header from "./Header"
+import HorizontalFoodCard from "./HorizontalFoodCard"
+import IconButton from "./IconButton"
+import TextButton from "./TextButton";
+import TextIconButton from "./TextIconButton"
+import TwoPointSlider from "./TwoPointSlider"
+import VerticalFoodCard from "./VerticalFoodCard"
 
 export {
-  Header,
-  HorizontalFoodCart,
+    Header,
+    HorizontalFoodCard,
+    IconButton,
+    TextButton,
+    TextIconButton,
+    TwoPointSlider,
+    VerticalFoodCard
 }
